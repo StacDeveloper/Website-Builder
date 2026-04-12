0c789464182ea3a50bbb66098c82299a698a347f
Soham Mule <sohammule09@gmail.com>
2026-04-12 13:43:09 +0100

    done

--- frontend/src/pages/Preview.tsx
+++ frontend/src/pages/Preview.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { Loader2Icon } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyProjects } from '../types/assets'
 
 const Preview = () => {
+  const { projectId, versionId } = useParams()
+  const [code, SetCode] = useState<string>("")
+  const [loading, SetLoading] = useState<boolean>(true)
+
+
+  const fetchCode = async () => {
+    const code = dummyProjects.find((proj) => proj.id === projectId)?.current_code
+    setTimeout(() => {
+      if (code) {
+        SetCode(code)
+        SetLoading(false)
+      }
+    }, 2000);
+  }
+
+  useEffect(() => {
+    fetchCode()
+  }, [])
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader2Icon className='size-7 animate-spin text-indigo-200' />
+      </div>
+    )
+  }
   return (
-    <div>Preview</div>
+    <div className='h-screen'>
+
+    </div>
   )
 }
 
--- frontend/src/pages/View.tsx
+++ frontend/src/pages/View.tsx
@@ -1,12 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { dummyProjects } from '../types/assets'
+import { Loader2Icon } from 'lucide-react'
+import ProjectPreview from '../components/ProjectPreview'
+import type { Project } from '..'
 
 const View = () => {
-  const {projectId} = useParams()
+  const { projectId } = useParams()
   const [code, SetCode] = useState<string>("")
-  const [loading, SetLoading] = useState<boolean>(true) 
+  const [loading, SetLoading] = useState<boolean>(true)
+
+  const fetchCode = async () => {
+    const code = dummyProjects.find((proj) => proj.id === projectId)?.current_code
+
+    setTimeout(() => {
+      if (code) {
+        SetCode(code)
+        SetLoading(false)
+      }
+    }, 2000);
+  }
+
+  useEffect(() => {
+    fetchCode()
+  }, [])
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader2Icon className='size-7 animate-spin text-indigo-200' />
+      </div>
+    )
+  }
+
   return (
-    <div>View</div>
+    <div>
+      {code && <ProjectPreview project={{ current_code: code } as Project} isGenerating={false} showEditorPanel={false} />}
+    </div>
   )
 }
 
