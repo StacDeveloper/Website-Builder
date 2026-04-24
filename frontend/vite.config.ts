5314b7bd1eab35a814c3c20a02100b55c7e81d92
Soham Mule <sohammule09@gmail.com>
2026-04-24 18:51:15 +0100

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:3000",
+        target: "https://website-builder-seven-indol.vercel.app/",
         changeOrigin: true,
       }
     }
